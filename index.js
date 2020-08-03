53782adc74315298548ea47fdf4b1de5d98ca3ee
Dan Mahavithana <dan@fingermark.tech>
2020-08-03 12:12:01 +1200

    cleanded up and added bashcommand flag

--- index.js
+++ index.js
@@ -1,56 +1,65 @@
 const AWS  = require('aws-sdk');
 var exec = require('child_process').exec;
+
 var argv = require('minimist')(process.argv.slice(2));
 const clipboardy = require('clipboardy');
 
 
+const {arnrole, sessionname, duration, verbouse, role, accountnumber, bashcommand=""} = argv
 
-const {arnrole, sessionname, duration, verbouse} = argv
-
-
-
-const S3 = new AWS.S3();
 const sts = new AWS.STS()
 
-
-// const S3BUCKETS = S3.listBuckets((err, data)=>{
-//     if(err){
-//         console.log('error', err)
-//         return err;
-//     }
-
-//     console.log('data', data)
-    
-    
-// })
+if(!(arnrole || accountnumber)){
+    console.log('--arnrole or --accountnumber flags are required')
+    return
+}
 
 var roleToAssume = {
-    RoleArn: arnrole,//'arn:aws:iam::429308279912:role/temporary_access_to_devs',
+    RoleArn: arnrole || `arn:aws:iam::${accountnumber}:role/${role || "temporary_access_to_devs"}`,
     RoleSessionName: sessionname || "Temp Access"//'dan@QA',
   };
 
 
 
-const data1 = sts.assumeRole(roleToAssume, (err,data)=>{
+const data1 = sts.assumeRole(roleToAssume, async (err,data)=>{
     if(err)
         return err
-    console.log("Just paste and press enter");
-    if(verbouse){
-        console.log("if any issues manually COPY this and run to update the environement variables")
-        console.log(`export AWS_ACCESS_KEY_ID=${data.Credentials.AccessKeyId}; export AWS_SECRET_ACCESS_KEY=${data.Credentials.SecretAccessKey}; export AWS_SESSION_TOKEN=${data.Credentials.SessionToken}`)
-    }
+    
+
+    if(bashcommand){
+        process.env['AWS_ACCESS_KEY_ID'] = `${data.Credentials.AccessKeyId}`
+        process.env['AWS_SECRET_ACCESS_KEY'] = `${data.Credentials.SecretAccessKey}`
+        process.env['AWS_SESSION_TOKEN'] = `${data.Credentials.SessionToken}`
+    
+    
+        await exec(`${bashcommand}`,(err,output, outputerr)=>{
+            if (err) {
+                console.log(`error: ${err.message}`);
+                return;
+            }
+            if (outputerr) {
+                console.log(`stderr: ${outputerr}`);
+                return;
+            }
+            console.log(`output: ${output}`);
+    
+        }) 
+        console.log("Access credentials have also been copied to your clipboard.");
+
+        if(verbouse){
+            console.log("if any issues manually COPY this and run to update the environement variables")
+            console.log(`export AWS_ACCESS_KEY_ID=${data.Credentials.AccessKeyId}; export AWS_SECRET_ACCESS_KEY=${data.Credentials.SecretAccessKey}; export AWS_SESSION_TOKEN=${data.Credentials.SessionToken}`)
+        }
         
-    clipboardy.writeSync(`export AWS_ACCESS_KEY_ID=${data.Credentials.AccessKeyId}; export AWS_SECRET_ACCESS_KEY=${data.Credentials.SecretAccessKey}; export AWS_SESSION_TOKEN=${data.Credentials.SessionToken}`);
+        clipboardy.writeSync(`export AWS_ACCESS_KEY_ID=${data.Credentials.AccessKeyId}; export AWS_SECRET_ACCESS_KEY=${data.Credentials.SecretAccessKey}; export AWS_SESSION_TOKEN=${data.Credentials.SessionToken}`);
 
-
-    // exec(`echo export AWS_ACCESS_KEY_ID=${data.Credentials.AccessKeyId}; echo export AWS_SECRET_ACCESS_KEY=${data.Credentials.SecretAccessKey}; echo export AWS_SESSION_TOKEN=${data.Credentials.SessionToken}`, (err, stdout, stderr) => {
-    //     if (err) {
-    //       //some err occurred
-    //       console.error(err)
-    //     } else {
-    //      // the *entire* stdout and stderr (buffered)
-    //      console.log(`stdout: ${stdout}`);
-
-    //     }
-    //   });
+    }else{
+        console.log("Just paste and press enter");
+        if(verbouse){
+            console.log("if any issues manually COPY this and run to update the environement variables")
+            console.log(`export AWS_ACCESS_KEY_ID=${data.Credentials.AccessKeyId}; export AWS_SECRET_ACCESS_KEY=${data.Credentials.SecretAccessKey}; export AWS_SESSION_TOKEN=${data.Credentials.SessionToken}`)
+        }
+        
+        clipboardy.writeSync(`export AWS_ACCESS_KEY_ID=${data.Credentials.AccessKeyId}; export AWS_SECRET_ACCESS_KEY=${data.Credentials.SecretAccessKey}; export AWS_SESSION_TOKEN=${data.Credentials.SessionToken}`);
+    }
 })
